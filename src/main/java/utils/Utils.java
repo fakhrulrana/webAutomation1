40fa4f7a761c34ec52da17df9461d085b50154ea
DESKTOP-0Q7MHGK\RSCS <fakhrulcsedu@gmail.com>
2024-02-13 02:53:43 +0600

    no message

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,5 +1,12 @@
 package utils;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Utils {
 
     public int generateRndmNumber()
@@ -12,5 +19,33 @@ public class Utils {
 
         System.out.println("Random value of type double between "+min+" to "+max+ ":" +randomNumber);
         return randomNumber;
+    }
+
+    public void captureScreenShot(WebDriver driver, String path) throws Exception {
+// TODO Auto-generated method stub
+        System.setProperty("webdriver.chrome.driver", "./src/resources/chromedriver.exe");
+//System.setProperty("webdriver.gecko.driver", "E:\\Selenium-Softwares\\geckodriver.exe");
+        driver=new ChromeDriver();
+        driver.get("http://demo.automationtesting.in/Register.html");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        WebElement element = driver.findElement(By.xpath(path));
+
+        File source = ((TakesScreenshot)element).getScreenshotAs(OutputType.FILE);
+        try {
+            FileHandler.copy(source, new File("./screenshots/element.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+
     }
 }
